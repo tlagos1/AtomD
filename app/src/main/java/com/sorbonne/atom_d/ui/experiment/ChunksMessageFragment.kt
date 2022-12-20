fa059d6aaa69fc49852fae5fe1de78e5595ada30
tlagos1 <tomas.lagos-jenschke@lip6.fr>
2022-12-20 11:07:43 +0100

    bugs fixed

--- app/src/main/java/com/sorbonne/atom_d/ui/experiment/ChunksMessageFragment.kt
+++ app/src/main/java/com/sorbonne/atom_d/ui/experiment/ChunksMessageFragment.kt
@@ -22,6 +22,7 @@ class ChunksMessageFragment : Fragment()  {
         ExperimentViewModelFactory(DatabaseRepository(requireActivity().application))
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/com/sorbonne/atom_d/ui/experiment/ExperimentFragment.kt
+++ app/src/main/java/com/sorbonne/atom_d/ui/experiment/ExperimentFragment.kt
@@ -22,6 +22,10 @@ class ExperimentFragment : Fragment() {
         fun newInstance() = ExperimentFragment()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     private val viewModel: ExperimentViewModel by viewModels {
         ExperimentViewModelFactory(DatabaseRepository(requireActivity().application))
     }
